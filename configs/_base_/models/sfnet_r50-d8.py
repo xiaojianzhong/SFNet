bee6e71b3fb95a35ebf7932c58cd488191234357
azxj <zxj13413431649@gmail.com>
2021-08-27 05:40:13 +0800

    add SFNet-ResNet50 & SFNet-ResNet101

---
+++ configs/_base_/models/sfnet_r50-d8.py
@@ -0,0 +1,43 @@
+# model settings
+custom_imports = dict(imports=[
+    'sfnet.backbones.resnet',
+    'sfnet.necks.sf_neck',
+], allow_failed_imports=False)
+
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+sampler = dict(
+    type='OHEMPixelSampler',
+    thresh=0.7,
+    min_kept=10000)
+model = dict(
+    type='EncoderDecoder',
+    pretrained='open-mmlab://resnet18_v1c',
+    backbone=dict(
+        type='ResNetV1c',
+        stem_channels=128,
+        depth=50,
+        dilations=(1, 1, 2, 4),
+        norm_cfg=norm_cfg),
+    neck=dict(
+        type='SFNeck',
+        in_channels=[256, 512, 1024, 2048],
+        channels=256,
+        align_corners=True,
+        norm_cfg=norm_cfg),
+    decode_head=dict(
+        type='FCNHead',
+        in_channels=1024,
+        in_index=4,
+        channels=256,
+        num_convs=1,
+        kernel_size=3,
+        concat_input=False,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+        sampler=sampler,
+        align_corners=True),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
---
+++ configs/sfnet/sfnet_r101-d8_512x1024_50k_cityscapes.py
@@ -0,0 +1,14 @@
+_base_ = [
+    '../_base_/models/sfnet_r101-d8.py', '../_base_/datasets/cityscapes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_50k.py'
+]
+
+lr_config = dict(policy='poly', power=1.0, min_lr=1e-4, by_epoch=True)
+runner = dict(type='EpochBasedRunner', max_iters=None, max_epochs=300)
+checkpoint_config = dict(by_epoch=True, interval=30)
+evaluation = dict(interval=30, metric='mIoU')
+
+# fp16 settings
+optimizer_config = dict(type='Fp16OptimizerHook', loss_scale=512.)
+# fp16 placeholder
+fp16 = dict()
