a2112d4c14cbda0370cfdc4c23d4d82abc24da8e
azxj <zxj13413431649@gmail.com>
2021-08-27 04:49:36 +0800

    set align_corners to True

--- configs/_base_/models/sfnet_r18-d8.py
+++ configs/_base_/models/sfnet_r18-d8.py
@@ -22,6 +22,7 @@ model = dict(
         type='SFNeck',
         in_channels=[64, 128, 256, 512],
         channels=128,
+        align_corners=True,
         norm_cfg=norm_cfg),
     decode_head=dict(
         type='FCNHead',
@@ -35,7 +36,8 @@ model = dict(
         norm_cfg=norm_cfg,
         loss_decode=dict(
             type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
-        sampler=sampler),
+        sampler=sampler,
+        align_corners=True),
     # model training and testing settings
     train_cfg=dict(),
     test_cfg=dict(mode='whole'))
